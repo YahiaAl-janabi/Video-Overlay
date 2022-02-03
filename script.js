7fb79bd73c95248d2ce3ea2e68621209f04933a4
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2022-02-03 00:55:10 +0000

    added a preloader gif

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const switchBtn = document.querySelector('.switchBtn');
 const switchSpan = document.querySelector('.switch');
 const video = document.querySelector('.videoContainer');
+const preloader = document.querySelector('img');
 
-// window.addEventListener('DOMContentLoaded', function () {});
+window.addEventListener('load', function () {
+  preloader.classList.add('hidePreloader');
+});
 
 switchBtn.addEventListener('click', function () {
   if (!switchSpan.classList.contains('slide')) {
